f8c9f8aeb59bfc6b0e484f74766e9b434c963b6d
John Finlay <yalnifj@gmail.com>
2016-10-20 20:40:34 -0600

    facebooke

--- website/genquizitive.js
+++ website/genquizitive.js
@@ -2,9 +2,32 @@ angular.module('genquizitive', ['ui.bootstrap'])
 .controller('getstarted', function($scope, $uibModal) {
 	$scope.isLoggedIn = false;
 	
-	$scope.play = function() {
-		
-		FB.getLoginStatus(function(response) {
+	$scope.fs = new FamilySearch({
+	  //environment: 'production',
+	  environment: 'sandbox',
+	  appKey: 'a02j000000JERmSAAX',
+	  redirectUri: 'http://genquizitive.com/fsoauth.php',
+	  saveAccessToken: true,
+	  tokenCookie: 'FS_AUTH_TOKEN',
+	  maxThrottledRetries: 10
+	});
+	
+	$scope.fsLogin = function() {
+		$scope.fs.oauthRedirect();
+	};
+	
+	$scope.fsHandleOathResponse = function(response) {
+		if (response) {
+			console.log(response);
+			if (response.statusCode == 200) {
+				
+			}
+		}
+	};
+	
+	$scope.fbLogin = function() {
+		FB.login(function(response) {
+			console.log(response.status);
 			if (response.status === 'connected') {
 				// Logged into your app and Facebook.
 			} else if (response.status === 'not_authorized') {
@@ -13,11 +36,24 @@ angular.module('genquizitive', ['ui.bootstrap'])
 				// The person is not logged into Facebook, so we're not sure if
 				// they are logged into this app or not.
 			}
-		});
+		}, {scope: 'public_profile,email,user_friends,user_relationships'});
+	};
+	
+	$scope.play = function() {
 		
-		$scope.modalInstance = $uibModal.open({
-		  templateUrl: 'facebook-modal.html',
-		  scope: $scope
+		FB.getLoginStatus(function(response) {
+			console.log(response.status);
+			if (response.status === 'connected') {
+				// Logged into your app and Facebook.
+				$scope.fsLogin();
+			} else if (response.status === 'not_authorized') {
+				// The person is logged into Facebook, but not your app.
+				$scope.fbLogin();
+			} else {
+				// The person is not logged into Facebook, so we're not sure if
+				// they are logged into this app or not.
+				$scope.fbLogin();
+			}
 		});
 
 	};
