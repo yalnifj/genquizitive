50ad84062370a6f06dbf7a609ab53b823672a215
John Finlay <yalnifj@gmail.com>
2016-10-24 08:08:29 -0600

    trying to get a question

--- website/genquizitive.js
+++ website/genquizitive.js
@@ -90,10 +90,13 @@ angular.module('genquizitive', ['ngRoute','ngCookies','ui.bootstrap'])
 		return deferred.promise;
 	}
 }])
-.service('familysearchService', ['$q','$cookies', function($q, $cookies) {
+.service('familysearchService', ['$q','$cookies', '$interval', function($q, $cookies, $interval) {
 	this.fsUser = null;
 	
 	this.people = {};
+	this.portraitPeople = {};
+	this.usedPeople = {};
+	this.backgroundQueue = [];
 	
 	this.fs = new FamilySearch({
 	  //environment: 'production',
@@ -111,7 +114,10 @@ angular.module('genquizitive', ['ngRoute','ngCookies','ui.bootstrap'])
 		this.fs.get('/platform/tree/current-person', function(response){
 			if (response.statusCode==200) {
 				temp.fsUser = response.data.persons[0];
+				temp.usedPeople[temp.fsUser.id] = true;
 				temp.getAncestorTree(temp.fsUser.id);
+				temp.getPersonPortrait(temp.fsUser.id);
+				this.startBackgroundQueue();
 				deferred.resolve(temp.fsUser);
 			} else {
 				deferred.reject(response.body);
@@ -127,30 +133,144 @@ angular.module('genquizitive', ['ngRoute','ngCookies','ui.bootstrap'])
 	this.fsLoginComplete = function(response) {
 		var token = $cookies.get(this.fs.tokenCookie);
 		this.fs.setAccessToken(token);
+		this.startBackgroundQueue();
 		this.loginWin.close();
+	};
+	
+	this.startBackgroundQueue = function() {
+		if (this.interval) {
+			$interval.cancel(this.interval);
+		}
+		var temp = this;
+		this.interval = $interval(function() {
+			if (temp.backgroundQueue.length > 0) {
+				var func = temp.backgroundQueue.shift();
+				func();
+			}
+		}, 1000);
+	};
+	
+	this.stopBackgroundQueue = function() {
+		if (this.interval) {
+			$interval.cancel(this.interval);
+		}
+		this.interval = null;
 	};
 	
 	this.getAncestorTree = function(personId, noCache) {
 		var deferred = $q.defer();
 		var temp = this;
-		this.fs.get('/platform/tree/ancestry?person='+personId, function(response) {
+		this.fs.get('/platform/tree/ancestry?person='+personId+'&generations=6&personDetails=', function(response) {
 			if (!noCache) {
 				for(var p=0; p < response.data.persons.length; p++) {
 					var person = response.data.persons[p];
 					temp.people[person.id] = person;
+					temp.backgroundQueue.push(function(){temp.getPersonPortrait(person.id)});
 				}
 			}
 			deferred.resolve(response.data);
 		});
 		return deferred.promise;
 	};
+	
+	this.getPersonPortrait = function(personId) {
+		var deferred = $q.defer();
+		if (this.people[personId] && this.people[personId].portrait) {
+			deferred.resolve(this.people[personId].portrait);
+		}
+		
+		var temp = this;
+		this.fs.get('/platform/tree/persons/'+personId+'/portrait', function(response) {
+			if (response.statusCode==200 || response.statusCode == 307) {
+				if (temp.people[personId]) {
+					temp.people[personId].portrait = response.getHeader('Content-Location');
+				}
+				temp.portraitPeople[personId] = true;
+				deferred.resolve(response.getHeader('Content-Location'));
+			} else {
+				deferred.reject(response.data);
+			}
+		});
+		
+		return deferred.promise;
+	};
+	
+	this.getRandomPersonWithPortrait = function() {
+		var deferred = $q.defer();
+		if (this.portraitPeople.length > 0) {
+			var count = 0;
+			var person = null;
+			while(count < 5 && person == null) {
+				var keys = Object.keys(this.portraitPeople);
+				var rand = Math.floor(Math.random() * keys.length);
+				var randomId = keys[rand];
+				if (!usedPeople[randomId]) {
+					person = randomId;
+				}
+			}
+			if (person != null) {
+				deferred.resolve(person);
+				return deferred.promise;
+			}
+		}
+		if (this.people.length > 0) {
+			var count = 0;
+			var person = null;
+			while(count < 5 && person == null) {
+				var keys = Object.keys(this.people);
+				var rand = Math.floor(Math.random() * keys.length);
+				var randomId = keys[rand];
+				if (!usedPeople[randomId]) {
+					person = randomId;
+				}
+			}
+			if (person != null) {
+				this.getPersonPortrait(person).then(function() {
+						deferred.resolve(person);
+					}, function(error) {
+						deferred.reject('No portrait for '+person);
+					});
+			}
+			deferred.reject('No people available');
+		} else {
+			deferred.reject('No people loaded');
+		}
+			
+		return deferred.promise;
+	};
+	
+	this.getRandomPerson = function() {
+		var deferred = $q.defer();
+		if (this.people.length > 0) {
+			var count = 0;
+			var person = null;
+			while(count < 5 && person == null) {
+				var keys = Object.keys(this.people);
+				var rand = Math.floor(Math.random() * keys.length);
+				var randomId = keys[rand];
+				if (!usedPeople[randomId]) {
+					person = randomId;
+				}
+			}
+			if (person != null) {
+				deferred.resolve(person);
+			} else {
+				deferred.reject ('No people available')
+			}
+		} else {
+			deferred.reject('No people loaded');
+		}
+		return deferred.promise;
+	}
 }])
 .service('notificationService', ['$rootScope', '$compile', function($rootScope, $compile){
-	this.showNotification = function(title, message) {
+	this.showNotification = function(options) {
 		var $scope = $rootScope.$new();
-		$scope.title = title;
-		$scope.message = message;
-		var template = '<div class="notification"><div class="title">{{title}}</div><div class="message">{{message}}</div></div>';
+		$scope.title = options.title;
+		$scope.message = options.message;
+		$scope.options = options;
+		var template = '<div class="notification"><div class="title">{{title}}</div><div class="message">{{message}}</div>\
+			<button ng-if="options.closable" ng-click="close()" class="btn btn-default closebutton">Close</button></div>';
 		$scope.element = $compile(template)($scope);
 		$('body').append($scope.element);
 		
@@ -160,12 +280,22 @@ angular.module('genquizitive', ['ngRoute','ngCookies','ui.bootstrap'])
 			$scope.element.animate({top: '20px'}, 1500);
 		};
 		
-		$scope.hide = function() {
+		$scope.close = function() {
 			$scope.element.animate({top: '-500px'}, 1000).remove();
 		};
 		
 		return $scope;
 	};
+}])
+.service ('QuestionService', [function() {
+	this.question = [
+		{name: 'photo1', difficulty: 1}
+	];
+	
+	this.getRandomQuestion = function () {
+		var q = Msth.floor (Math.random () * this.question.length);
+		return this.questions [q];
+	}
 }])
 .directive('countdown', ['$interval', '$timeout', function($interval, $timeout) {
 	return {
@@ -217,7 +347,29 @@ angular.module('genquizitive', ['ngRoute','ngCookies','ui.bootstrap'])
 		}
 	}
 }])
-.controller('getstarted', function($scope, familysearchService, facebookService, $location, $timeout) {
+.directive('question', ['QuestionService', '$compile', function (QuestionService, $compile) {
+	return {
+		scope: {
+			question: '='
+		},
+		link ($scope, $element, $attr) {
+			$scope.loadQuestion = function () {
+				$scope.$emit('changeBackground', 'questions/'+$scope.question.name+'/background.jpg');
+				
+				var templateUrl = 'questions/'+$scope.question.name+'/question.html';
+				var qel = $compile(templateUrl)($scope);
+				$element.append(qel);
+			}
+			
+			$scope.$watch('question', function (newval, oldval) {
+				if (newval && newval!=oldval) {
+					$scope.loadQuestion();
+				}
+			});
+		}
+	}
+}])
+.controller('getstarted', function($scope, familysearchService, facebookService, $location, $timeout, notificationService) {
 	$scope.$emit('changeBackground', 'home_background.jpg');
 	$scope.fsLoggedIn = false;
 	$scope.fsUserName = "";
@@ -230,6 +382,9 @@ angular.module('genquizitive', ['ngRoute','ngCookies','ui.bootstrap'])
 				$scope.fsLoggedIn = true;
 				$scope.fsUserName = fsUser.display.name;
 				$scope.checkLogin();
+			}, function(error) {
+				var notif = notificationService.showNotification({title: 'FamilySearch Error',message: 'Unable to authenticate with FamilySearch.', closable: true});
+				notif.show();
 			});
 		}, 500);
 	});
@@ -284,12 +439,15 @@ angular.module('genquizitive', ['ngRoute','ngCookies','ui.bootstrap'])
 		{route: '/challenge', title: 'Challenge a Friend', button: 'right'}
 	];
 })
-.controller('practiceController', function($scope, notificationService) {
-	var notif = notificationService.showNotification('Practice Round','Answer the questions as quickly as you can.');
+.controller('practiceController', function($scope, notificationService, QuestionService) {
+	$scope.$emit('changeBackground', 'home_background.jpg');
+	
+	var notif = notificationService.showNotification({title: 'Practice Round', message: 'Answer the questions as quickly as you can.'});
 	notif.show();
 	
 	$scope.startRound = function() {
-		
+		notif.close();
+		var question = QuestionService.getRandomQuestion();
 	}
 })
 ;
