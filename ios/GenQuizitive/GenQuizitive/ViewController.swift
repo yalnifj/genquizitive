e0c20563e99a1cad2671d48118c86ff70fa4c265
yalnifj <yalnifj@gmail.com>
2016-12-01 07:49:04 -0700

    iOS genesis

--- ios/GenQuizitive/GenQuizitive/ViewController.swift
+++ ios/GenQuizitive/GenQuizitive/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var arrows: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var arrowsArr = [UIImage]()
+        arrowsArr.append(UIImage(named: "home_arrow1")!)
+        arrowsArr.append(UIImage(named: "home_arrow2")!)
+        arrowsArr.append(UIImage(named: "home_arrow3")!)
+        arrows.animationImages = arrowsArr
+        arrows.animationDuration = 0.3
+        arrows.animationDuration = 1.0
+        arrows.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
